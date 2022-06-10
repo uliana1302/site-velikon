f799532c431a633ad64bc19694259e1c0b4bc399
uliana1302 <uvvorobyova@edu.hse.ru>
2022-06-10 14:31:47 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,51 @@
 $(function () {
 
+ // появление объектов на главной странице
  setTimeout( "jQuery('.h').show();",14000 );
+
+ // появление названий разделов при наведении
+ $(".block-chemodan").hover(function(){
+   $(".p-chemodan").css("opacity", "1");
+ });
+ $(".block-map").hover(function(){
+   $(".p-map").css("opacity", "1");
+ });
+ $(".block-hand").hover(function(){
+   $(".p-hand").css("opacity", "1");
+ });
+ $(".block-sign").hover(function(){
+   $(".p-sign").css("opacity", "1");
+ });
+ $(".block-pugov").hover(function(){
+   $(".p-pugov").css("opacity", "1");
+ });
+ $(".block-kolon").hover(function(){
+   $(".p-kolon").css("opacity", "1");
+ });
+ $(".block-korzina").hover(function(){
+   $(".p-korzina").css("opacity", "1");
+ });
+
+ $(".block-chemodan").mouseleave(function(){
+   $(".p-chemodan").css("opacity", "0");
+ });
+ $(".block-map").mouseleave(function(){
+   $(".p-map").css("opacity", "0");
+ });
+ $(".block-hand").mouseleave(function(){
+   $(".p-hand").css("opacity", "0");
+ });
+ $(".block-sign").mouseleave(function(){
+   $(".p-sign").css("opacity", "0");
+ });
+ $(".block-pugov").mouseleave(function(){
+   $(".p-pugov").css("opacity", "0");
+ });
+ $(".block-kolon").mouseleave(function(){
+   $(".p-kolon").css("opacity", "0");
+ });
+ $(".block-korzina").mouseleave(function(){
+   $(".p-korzina").css("opacity", "0");
+ });
 
 });
